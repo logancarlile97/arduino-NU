fd97cd6d3fc9b34ee103d8f8f31abd122c40c986
Logan Carlile <logancarlile97@gmail.com>
2022-11-26 21:37:35 -0500

    Fixed error with xcpng script

--- arduino-NU-client-xcpng.sh
+++ arduino-NU-client-xcpng.sh
@@ -2,43 +2,6 @@
 
 # Usage arduino-NU-client-linux.sh <ip_address_to_arduinoNU_server> <seconds_offline_before_shutdown>
 
-# Set the ip address as the first passed argument.
-arduinoNUIP=$1
-
-# set time offline to shutdown equal to second passed argument
-timeOfflineToShutdown=$2
-
-# IP address of the arduino UPS monitor server
-arduinoNUServer=$arduinoNUIP/timeOffline
-
-timeOffline=`curl -sL --connect-timeout 15 $arduinoNUServer`
-
-if [ $timeOffline -ge $timeOfflineToShutdown ]; then
-    echo Arduino NU has reported UPS is offline for $timeOfflineToShutdown seconds
-    echo Shutting down in  10 seconds
-    sleep 10
-    
-    # Check that host has master role if not master master will initiate shutdown
-    role="$(more /etc/xensource/pool.conf)"
-    if [[ $role == *"master"* ]]; then
-        xcpng-shutdown
-    fi
-
-elif [ $timeOffline -ge 1 ]; then
-    echo Arduino NU has reported UPS is offline for $timeOffline seconds
-    echo System will shutdown after Arduino NU reports offline for $timeOfflineToShutdown seconds
-
-elif [ $timeOffline -eq 0 ]; then
-    echo Arduino NU has reported UPS is online
-
-else
-	# Tell user status could not be detected
-	echo Arduino NU status could not be determined
-
-fi
-
-
-# all below is script to safely shutdown xcpng
 # This script will shutdown all running VMs in a Xen pool, all slave Xen 
 # hosts, and eventually the Xen pool master. 
 #
@@ -193,3 +156,38 @@ log_date () {
 	# logging function formatted to include a date
 	echo -e "$(date "+%Y/%m/%d %H:%M:%S"): $1" >> "$log_file" #2>&1
 }
+
+# Set the ip address as the first passed argument.
+arduinoNUIP=$1
+
+# set time offline to shutdown equal to second passed argument
+timeOfflineToShutdown=$2
+
+# IP address of the arduino UPS monitor server
+arduinoNUServer=$arduinoNUIP/timeOffline
+
+timeOffline=`curl -sL --connect-timeout 15 $arduinoNUServer`
+
+if [ $timeOffline -ge $timeOfflineToShutdown ]; then
+    echo Arduino NU has reported UPS is offline for $timeOfflineToShutdown seconds
+    echo Shutting down in  10 seconds
+    sleep 10
+    
+    # Check that host has master role if not master master will initiate shutdown
+    role="$(more /etc/xensource/pool.conf)"
+    if [[ $role == *"master"* ]]; then
+        xcpng-shutdown
+    fi
+
+elif [ $timeOffline -ge 1 ]; then
+    echo Arduino NU has reported UPS is offline for $timeOffline seconds
+    echo System will shutdown after Arduino NU reports offline for $timeOfflineToShutdown seconds
+
+elif [ $timeOffline -eq 0 ]; then
+    echo Arduino NU has reported UPS is online
+
+else
+	# Tell user status could not be detected
+	echo Arduino NU status could not be determined
+
+fi
